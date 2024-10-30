1b3e4ca471fcabc7174d760df815464ac2fa0931
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-10-30 16:14:11 +0800

    chore: update

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,3 +1,15 @@
-import { defineConfig } from "sponsorkit";
+import { defineConfig, tierPresets } from 'sponsorkit'
 
-export default defineConfig({});
+export default defineConfig({
+  tiers: [
+    {
+      title: 'Active Sponsors',
+      preset: tierPresets.xs,
+    },
+    {
+      title: 'Past Sponsors',
+      monthlyDollars: -1,
+      preset: tierPresets.xs,
+    },
+  ],
+})
