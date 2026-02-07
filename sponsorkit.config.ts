9e53963ace0519f9ac48d18adc7d0e5fde727afd
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-02-07 23:30:05 +0800

    feat: show sponsoring

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig, tierPresets } from 'sponsorkit'
+import process from 'node:process'
+
+const mode = process.env.SPONSORKIT_MODE as 'sponsor' | 'sponsoring'
 
 export default defineConfig({
-  tiers: [
+  mode,
+  tiers: mode ? [
     {
       title: 'Active Sponsors',
       preset: tierPresets.xs,
@@ -11,5 +15,6 @@ export default defineConfig({
       monthlyDollars: -1,
       preset: tierPresets.xs,
     },
-  ],
+  ] : [],
+  outputDir: `./sponsorkit/${mode}`,
 })
